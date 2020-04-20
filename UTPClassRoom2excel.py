83cf1bee5e2667150f922c9beae2b703bf5cda47
Adrian Ramirez <lobosoli@users.noreply.github.com>
2020-04-20 10:23:48 -0500

    Update UTPClassRoom2excel.py

--- UTPClassRoom2excel.py
+++ UTPClassRoom2excel.py
@@ -20,9 +20,9 @@ SCOPES = ['https://www.googleapis.com/auth/classroom.courses.readonly',
         'https://www.googleapis.com/auth/classroom.rosters.readonly',
         'https://www.googleapis.com/auth/classroom.profile.emails',
         'https://www.googleapis.com/auth/classroom.profile.photos',
-	'https://www.googleapis.com/auth/drive', 
+	    'https://www.googleapis.com/auth/drive', 
         'https://www.googleapis.com/auth/spreadsheets',
-	'https://www.googleapis.com/auth/spreadsheets.readonly']
+	    'https://www.googleapis.com/auth/spreadsheets.readonly']
 
 def check_auth(api, version):
     creds = None
@@ -37,7 +37,7 @@ def check_auth(api, version):
     if os.path.exists('token.pickle'):
         with open('token.pickle', 'rb') as token:
             creds = pickle.load(token)
-            
+
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
@@ -64,7 +64,7 @@ def main():
     # Number of rows   
     contar= 1
 
-    if os.path.isfile("ClassRoomNumStudents5.xlsx"):
+    if os.path.isfile("ClassRoomNumStudents.xlsx"):
         print("ERROR: El Archivo ya existe")
     else:
         wb = openpyxl.Workbook()
@@ -83,39 +83,40 @@ def main():
     
     if not courses:
         print('No courses found.')
-    else:                       
-        for course in courses:
-            page_token = results.get('nextPageToken',None)            
-            while page_token != None:
-                for course in courses:
-                    #idcurso = course['id']
-                    #idprofesor = course.get('ownerId')
-                    teacherID = classroom_service.userProfiles().get(userId = course.get('ownerId'), x__xgafv='2', alt="json").execute()
-                    Estudiantes = classroom_service.courses().students().list(courseId = course['id'], pageSize = 0, x__xgafv='2', alt="json").execute()
-                    datosEstudiantes = Estudiantes.get('students',[])
+    else:               
+        #print('ID Curso;Nombre Curso;ID Profesor;Nombre Profesor;Correo Profesor;Fecha de Creación;Ultima Actualización;Número de Estudiantes;Estado del Curso')
+        page_token = results.get('nextPageToken',None)            
+        while page_token != "":
+            for course in courses:
+                #idcurso = course['id']
+                #idprofesor = course.get('ownerId')
+                teacherID = classroom_service.userProfiles().get(userId = course.get('ownerId'), x__xgafv='2', alt="json").execute()
+                Estudiantes = classroom_service.courses().students().list(courseId = course['id'], pageSize = 0, x__xgafv='2', alt="json").execute()
+                datosEstudiantes = Estudiantes.get('students',[])
+                estuCont = 0
+                contar=contar + 1
+                # Get the numbers of students
+                if not datosEstudiantes:
                     estuCont = 0
-                    contar=contar + 1
-                    #Get the numbers of students
-                    if not datosEstudiantes:
-                        estuCont = 0
-                    else:
-                        for listaEstudiantes in datosEstudiantes:                            
-                            estuCont = estuCont + 1                  
+                else:
+                    for listaEstudiantes in datosEstudiantes:                            
+                        estuCont = estuCont + 1
+                #print(course['id'],';', course['name'],';', course['ownerId'],';', teacherID['name']['fullName'],';', teacherID['emailAddress'],';', course['creationTime'],';', course['updateTime'],';', estuCont ,';', course['courseState'])                              
 
-                    ws.cell(column = 1, row = contar, value ="{0}".format(str(course['id'])))
-                    ws.cell(column = 2, row = contar, value ="{0}".format(str(course['name'])))
-                    ws.cell(column = 3, row = contar, value ="{0}".format(str(course['ownerId'])))
-                    ws.cell(column = 4, row = contar, value ="{0}".format(str(teacherID['name']['fullName'])))
-                    ws.cell(column = 5, row = contar, value ="{0}".format(str(teacherID['emailAddress'])))
-                    ws.cell(column = 6, row = contar, value ="{0}".format(str(course['creationTime'])))
-                    ws.cell(column = 7, row = contar, value ="{0}".format(str(course['updateTime'])))
-                    ws.cell(column = 8, row = contar, value ="{0}".format(str(estuCont)))
-                    ws.cell(column = 9, row = contar, value ="{0}".format(str(course['courseState'])))
-                    wb.save(filename = "ClassRoomNumStudents5.xlsx")                    
+                ws.cell(column = 1, row = contar, value ="{0}".format(str(course['id'])))
+                ws.cell(column = 2, row = contar, value ="{0}".format(str(course['name'])))
+                ws.cell(column = 3, row = contar, value ="{0}".format(str(course['ownerId'])))
+                ws.cell(column = 4, row = contar, value ="{0}".format(str(teacherID['name']['fullName'])))
+                ws.cell(column = 5, row = contar, value ="{0}".format(str(teacherID['emailAddress'])))
+                ws.cell(column = 6, row = contar, value ="{0}".format(str(course['creationTime'])))
+                ws.cell(column = 7, row = contar, value ="{0}".format(str(course['updateTime'])))
+                ws.cell(column = 8, row = contar, value ="{0}".format(str(estuCont)))
+                ws.cell(column = 9, row = contar, value ="{0}".format(str(course['courseState'])))
+                wb.save(filename = "ClassRoomNumStudents.xlsx")                    
                 
-                page_token = results.get('nextPageToken',None)
-                results = classroom_service.courses().list(pageToken = page_token, pageSize = 500).execute()
-                courses = results.get('courses', [])
+            page_token = results.get('nextPageToken',None)
+            results = classroom_service.courses().list(pageToken = page_token, pageSize = 10).execute()
+            courses = results.get('courses', [])
          
 
 if __name__ == '__main__':
